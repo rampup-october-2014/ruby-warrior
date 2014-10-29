acfce75e83e8d1e7304d760231bfa197728ad043
Peter White <pwhite007@live.com>
2014-10-29 13:26:01 -0400

    First three levels.

---
+++ level1.rb
@@ -0,0 +1,7 @@
+#Level 1:
+
+class Player
+  def play_turn(warrior)
+    warrior.walk!
+  end
+end
---
+++ level2.rb
@@ -0,0 +1,12 @@
+#Level 2:
+
+
+class Player
+  def play_turn(warrior)
+    if warrior.feel.enemy?
+      warrior.attack!
+    else
+    warrior.walk!
+  end
+  end
+end
